8925be8fca5acf550337d2f832148fe01a205a5f
DevinOh7 <baughn.devin@me.com>
2025-05-04 18:20:24 -0400

    Click to Move Grid Movement
    Modfiied player.gd Script.
    	-Added click to move movement
    	-Movement paths around tiles that don’t have “walkable” set to TRUE
    
    Added path.tscn Scene
    Added path.gd Script
    	Only Purpose of this Scene/Script is to draw a visual line showing the path being taken

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,27 +7,53 @@ class_name Player extends CharacterBody2D
 
 const tile_size: Vector2 = Vector2(16,16)
 var sprite_node_pos_tween: Tween
- 
+
+
+#Additional Variables for Click Based Movement
+var astar_grid: AStarGrid2D
+var current_id_path: Array[Vector2i]
+var current_point_path: PackedVector2Array
+var target_position: Vector2
+var is_moving: bool #part of logic to allow clicking when moving
+
 func _physics_process(_delta: float) -> void:
 	#make sure tween is not currently moving the sprite
 	if !sprite_node_pos_tween or !sprite_node_pos_tween.is_running():
 		#make sure attack animation is not playing
 		if !isAttacking:
-			#return to idle position animation
-			animatedSprite.play("idle");
+			#IF ID PATH IS EMPTY. ALLOW WASD TO MOVE
+			if current_id_path.is_empty():
+				#return to idle position animation
+				animatedSprite.play("idle");
+				#check for user inputs
+				if Input.is_action_pressed("up"):
+					_move(Vector2(0,-1))
+				elif Input.is_action_pressed("down"):
+					_move(Vector2(0,1))
+				elif Input.is_action_pressed("left"):
+					_move(Vector2(-1,0))
+				elif Input.is_action_pressed("right"):
+					_move(Vector2(1,0))
+				elif !isAttacking && Input.is_action_just_pressed("attack"):
+					pass #Code to do the Attack Animation Stuff
+				return;
 			
-			#check for user inputs
-			if Input.is_action_pressed("up"):
-				_move(Vector2(0,-1))
-			elif Input.is_action_pressed("down"):
-				_move(Vector2(0,1))
-			elif Input.is_action_pressed("left"):
-				_move(Vector2(-1,0))
-			elif Input.is_action_pressed("right"):
-				_move(Vector2(1,0))
-			elif !isAttacking && Input.is_action_just_pressed("attack"):
-				pass #Code to do the Attack Animation Stuff
-			return;
+			#IF ID PATH IS NOT EMPTY. MOVE PLAYER TO TARGET POSITION
+			if is_moving == false:
+				target_position = tile_map.map_to_local(current_id_path.front())
+				is_moving = true
+				animatedSprite.play("run");
+
+			global_position = global_position.move_toward(target_position, 1)
+
+			if global_position == target_position:
+				
+				current_id_path.pop_front()
+				if current_id_path.is_empty() == false:
+					target_position = tile_map.map_to_local(current_id_path.front())
+				else:
+					is_moving = false
+		
 
 func _move(direction: Vector2):
 	# Get current tile Vector2i
@@ -41,8 +67,8 @@ func _move(direction: Vector2):
 	var tile_data: TileData = tile_map.get_cell_tile_data(0, target_tile)
 	
 # MAKE SURE TILE IS WALKABLE FIRST
-#	if tile_data.get_custom_data("walkable") == false:
-#		return
+	if tile_data.get_custom_data("walkable") == false:
+		return
 
 # MAKE SURE NO BODY Target is detected by the RAYCAST
 	ray_cast_2d.target_position = direction * 16
@@ -69,4 +95,52 @@ func _move(direction: Vector2):
 	
 #start player animation when game starts
 func _ready() -> void:
+	#Play Idle Animation on Player Start
 	animatedSprite.play("idle");
+	
+	#Setup Astar Grid
+	astar_grid = AStarGrid2D.new()
+	astar_grid.region = tile_map.get_used_rect()
+	astar_grid.cell_size = tile_size
+	astar_grid.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_NEVER
+	astar_grid.update()
+	
+	for x in tile_map.get_used_rect().size.x:
+		for y in tile_map.get_used_rect().size.y:
+			var tile_position = Vector2i(
+				x + tile_map.get_used_rect().position.x,
+				y + tile_map.get_used_rect().position.y			
+			)
+			
+			var tile_data = tile_map.get_cell_tile_data(0, tile_position)
+			
+			if tile_data == null or tile_data.get_custom_data("walkable") == false:
+				astar_grid.set_point_solid(tile_position)
+
+func _input(event):
+	if event.is_action_pressed("move") == false:
+		return
+	
+	var id_path
+	
+	if is_moving: #If moving, make sure next target square coordinates are grabbed and not current player coord.
+		id_path = astar_grid.get_id_path(
+			tile_map.local_to_map(target_position),
+			tile_map.local_to_map(get_global_mouse_position())
+		)
+	else:
+		id_path = astar_grid.get_id_path(
+			tile_map.local_to_map(global_position),
+			tile_map.local_to_map(get_global_mouse_position())
+		).slice(1)
+
+	if id_path.is_empty() == false:
+		current_id_path = id_path
+		
+		current_point_path = astar_grid.get_point_path(
+			tile_map.local_to_map(target_position),
+			tile_map.local_to_map(get_global_mouse_position())
+		)
+
+		for i in current_point_path.size():
+			current_point_path[i] = current_point_path[i] + tile_size / 2
